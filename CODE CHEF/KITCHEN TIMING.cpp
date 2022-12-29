6bc016eb0e027f62dd847f29882b0e9f67533340
ASHUTOSH MAURYA <114004121+Ashutoshmaurya21@users.noreply.github.com>
2022-12-29 15:46:50 +0530

    Create KITCHEN TIMING.cpp

---
+++ CODE CHEF/KITCHEN TIMING.cpp
@@ -0,0 +1,15 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+int n;
+cin>>n;
+while(n--){
+    int a,b;
+    cin>>a>>b;
+    cout<<abs(b-a);
+    cout<<endl;
+}
+	return 0;
+}
