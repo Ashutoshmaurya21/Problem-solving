d75fe690e025adaad7ee9487c9f81d4a5d01cd73
ASHUTOSH MAURYA <114004121+Ashutoshmaurya21@users.noreply.github.com>
2022-12-29 19:55:32 +0530

    Create PARITY.cpp

---
+++ CODE CHEF/PARITY.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    while(n--){
+        int a;
+        cin>>a;
+        if(a%2==0){
+        cout<<"YES";
+        }
+        else{
+            cout<<"NO";
+        }
+        cout<<endl;
+        
+    }
+	
+	return 0;
+}
