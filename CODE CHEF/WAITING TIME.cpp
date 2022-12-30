efb0c1559eb198cd561e7a4c01379c6aae658f60
ASHUTOSH MAURYA <114004121+Ashutoshmaurya21@users.noreply.github.com>
2022-12-30 23:34:37 +0530

    Create WAITING TIME.cpp

---
+++ CODE CHEF/WAITING TIME.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	while(n--){
+	    int a,b;
+	    cin>>a>>b;
+	    cout<<((a*7)-b);
+	    cout<<endl;
+	}
+	return 0;
+}
