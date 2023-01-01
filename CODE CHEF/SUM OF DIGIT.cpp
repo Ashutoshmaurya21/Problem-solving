3c75aa5a175d40b33680867c939b61947c1252f4
ASHUTOSH MAURYA <114004121+Ashutoshmaurya21@users.noreply.github.com>
2023-01-01 15:50:00 +0530

    Create SUM OF DIGIT.cpp

---
+++ CODE CHEF/SUM OF DIGIT.cpp
@@ -0,0 +1,29 @@
+//  We have populated the solutions for the 10 easiest problems for your support.
+// Click on the SUBMIT button to make a submission to this problem.
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        
+        int sum=0;
+        
+        while(n>0)
+        {
+            sum+=(n%10);
+            n/=10;
+        }
+        
+        cout<<sum<<"\n";
+        
+    }
+	return 0;
+}
