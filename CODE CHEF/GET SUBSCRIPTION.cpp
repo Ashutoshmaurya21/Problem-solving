68e17cfa81a6768a6393db6c7732607c1b70597a
ASHUTOSH MAURYA <114004121+Ashutoshmaurya21@users.noreply.github.com>
2023-01-03 19:10:11 +0530

    Create GET SUBSCRIPTION.cpp

---
+++ CODE CHEF/GET SUBSCRIPTION.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    while(n--){
+        int a;
+        cin>>a;
+        if(a>30){
+            cout<<"YES";
+        }else{
+            cout<<"NO";
+        }
+        cout<<endl;
+    }
+	// your code goes here
+	return 0;
+}
