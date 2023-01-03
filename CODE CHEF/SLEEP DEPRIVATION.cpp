bc35f10f4a6b7328ad8765f0878c928be2c82da1
ASHUTOSH MAURYA <114004121+Ashutoshmaurya21@users.noreply.github.com>
2023-01-04 00:50:38 +0530

    Create SLEEP DEPRIVATION.cpp

---
+++ CODE CHEF/SLEEP DEPRIVATION.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	while(n--){
+	    int a;
+	    cin>>a;
+	    if(a<7){
+	        cout<<"YES";
+	    }
+	    else{
+	        cout<<"NO";
+	    }
+	    cout<<endl;
+	}
+	return 0;
+}
