5bed2547c1b10f0461675436f2063290b65c0734
ASHUTOSH MAURYA <114004121+Ashutoshmaurya21@users.noreply.github.com>
2023-01-05 12:11:54 +0530

    Create APPLES AND ORANGE.cpp

---
+++ CODE CHEF/APPLES AND ORANGE.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	cout<<endl;
+	 long long a,b,r;
+	 cin>>a>>b;
+	 r=a+b;
+	 if(n>=r){
+	     cout<<"YES";
+	 }
+	 else{
+	     cout<<"NO";
+	 }
+	 
+	return 0;
+}
