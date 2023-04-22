22321e93307cfbe4042bdcce757ba16d0b1cb63a
ASHUTOSH MAURYA <114004121+Ashutoshmaurya21@users.noreply.github.com>
2023-04-22 10:17:09 +0530

    Create Different type of pattern in loop.cpp

---
+++ Different type of pattern in loop.cpp
@@ -0,0 +1,166 @@
+//first
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int i=1;i<=4;i++){
+        for(int j=1;j<=4;j++){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    }
+
+   return 0;
+}
+
+//second
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int i=1;i<=4;i++){
+        for(int j=1;j<=4;j++){
+        cout<<j<<" ";
+    }
+    cout<<endl;
+    }
+
+   return 0;
+}
+
+
+//third
+#include<iostream>
+using namespace std;
+int main()
+{
+    int count=1;
+    for(int i=1;i<=4;i++){
+        for(int j=1;j<=4;j++){
+        cout<<count<<" ";
+        count++;
+    }
+    cout<<endl;
+    }
+
+   return 0;
+}
+
+
+//fourth
+#include<iostream>
+using namespace std;
+int main()
+{
+    int count=1;
+    for(int i=1;i<=4;i++){
+        for(int j=1;j<=i;j++){
+        cout<<"*"<<" ";
+        
+    }
+    cout<<endl;
+    }
+
+   return 0;
+}
+
+//fifth
+#include<iostream>
+using namespace std;
+int main()
+{
+    int count=1;
+    for(int i=1;i<=4;i++){
+        for(int j=1;j<=i;j++){
+        cout<<i<<" ";
+        
+    }
+    cout<<endl;
+    }
+
+   return 0;
+}
+
+
+
+
+//six
+#include<iostream>
+using namespace std;
+int main()
+{
+    
+    for(int i=1;i<=4;i++){
+        int val=i;
+        for(int j=1;j<=i;j++){
+        
+        cout<<val<<" ";
+        val++;
+        
+    }
+    cout<<endl;
+    }
+
+   return 0;
+}
+
+
+//seven
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch='A';
+    for(int i=1;i<=4;i++){
+        
+        for(int j=1;j<=4;j++){
+        
+        cout<<ch<<" ";
+     }
+    cout<<endl;
+    ch++;
+    }
+
+   return 0;
+}
+
+//eight
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch='A';
+    for(int i=1;i<=4;i++){
+        
+        for(int j=1;j<=4;j++){
+           //char ch='A';
+        cout<<ch<<" ";
+        ch++;
+     }
+    cout<<endl;
+   
+    }
+
+   return 0;
+}
+
+
+//ten
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch='A';
+    for(int i=1;i<=4;i++){
+        
+        for(int j=1;j<=4;j++){
+           //char ch='A';
+        cout<<ch<<" ";
+        ch++;
+     }
+    cout<<endl;
+   
+    }
+
+   return 0;
+}
